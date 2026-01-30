3f84039f245cd4dbda7d385de367f8af01ad516b
Anusha0317 <anushareddys05170@gmail.com>
2026-01-30 13:55:05 +0000

    Add function to sum multiples of given factors

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
-alert("Hello World!");
+function sumMultiple(f,m){
+  let total=0;
+  for(let num of m){
+    for (let fact of f){
+      if (num % fact === 0) {
+        total += num;
+        break;
+      }
+    }
+  }
+  return total;
+}
+let f =[2,3];
+let m =[1,2,3,4,5,6,7,8,9];
+let result= sumMultiple(f,m);
+console.log(result);
